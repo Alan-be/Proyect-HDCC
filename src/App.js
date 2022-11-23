876a6963c54c56e8711b7f9361ff027dd0c8d83e
oscarPerezBee <oscar.perez@beesoftware.mx>
2022-11-23 14:49:43 -0600

    Poke Api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.scss';
 import { CardHeroes } from './Components/CardHeroes/CardHeroes';
 import { useEffect, useState } from 'react';
 import SearchBar from './Components/SearchBar/SearchBar';
-
-
+import './Components/PokeApi/style.css'
+import Main from './Components/PokeApi/Main';
+import NavBar from './Components/NavBar/NavBar'
 //https://gateway.marvel.com:443/v1/public/characters?ts=1apikey=65ec9ee4da9b3e7422fb540e63a19c57&hash=ce2e743870c3bef4cd5105a8f4e2ec2a
 
 
@@ -20,6 +21,7 @@ function App() {
     setCopy(responseJSON.data.results);
 
   }
+  
 
   useEffect(() => {
     fetchApi();
@@ -32,7 +34,8 @@ function App() {
 
   return (
     <div className="App">
-
+      <NavBar/>
+      <Main/>
 
     </div>
   );
