a0dd5a14eed91e829d7e8a46d298506977a227c7
oscarPerezBee <oscarj2901@gmail.com>
2022-11-24 17:33:40 -0600

    logos

--- src/Components/GoogleMaps/Maps.js
+++ src/Components/GoogleMaps/Maps.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import GoogleMaps from "simple-react-google-maps";
 import './Maps.scss'
+import GoogleLogo from '../../assets/images/googleLogo.png'
 export default class Maps extends Component {
   render() {
     return (
       <div className="containerMaps">
+
+        <div style={{display:"flex",alignItems:"center",alignContent:"center", width:"100%",justifyContent: "center", marginBottom:"2rem"}}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Google_Maps_Logo_2020.svg/2275px-Google_Maps_Logo_2020.svg.png" width="20%"/>
+        </div>
         <GoogleMaps
           apiKey={"AIzaSyC6hpIex4vd4paoVM75aOYYjc4uM4LTrv0"}
           style={{ height: "400px", width: "90%" }}
---
+++ src/Pages/GoogleMaps.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar'
+import Footer from './Components/Footer';
+import Main from '../Components/PokeApi/Main';
+
+const GoogleMaps = () => {
+    return(
+    <div>
+      <NavBar />
+      <Main />
+      <Footer/>      
+    </div>
+    )
+}
+
+
+
+export default GoogleMaps;
